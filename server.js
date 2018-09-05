3ae1f92e6f42ceb42caf05371679a5746d6b87f6
minggas <fasfora@gmail.com>
2018-09-05 00:48:09 -0300

    init

---
+++ server.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var express = require("express");
+var cors = require("cors");
+
+// require and use "multer"...
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+
+var app = express();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.get("/hello", function(req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (req.file) {
+    res.status(200).json({
+      filename: req.file.originalname,
+      size: `${req.file.size} bytes`,
+      type: req.file.mimetype
+    });
+  } else {
+    res
+      .status(500)
+      .json({ error: `No file was provided in the 'upfile' field` });
+  }
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
+});
